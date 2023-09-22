9a3309a4e03c6bed1eb0688d71f118833e436c77
Nick Urciuoli <nick.urciuoli@factset.com>
2023-09-22 12:06:08 -0400

    add id lookup and conc to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FdsPy",
-    version="0.4",
+    version="0.5",
     description="A library of common python scripts",
     packages=find_packages(),
 )
