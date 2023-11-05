b34717d6d5b04a5eec232d3690020f34ebc637b8
Nick Urciuoli <nick.urciuoli@factset.com>
2023-11-05 14:09:34 -0500

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FdsPy",
-    version="0.5",
+    version="0.6",
     description="A library of common python scripts",
     packages=find_packages(),
 )
